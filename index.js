55c4effd5e9a15c7d42231f5fad3eee4d03d33ce
Godsgift ThankGod Akari <giftakari@yahoo.com>
2020-06-08 07:29:35 +1000

    Starter files

---
+++ index.js
@@ -0,0 +1,6 @@
+const port = 3000;
+const htttp = require("http");
+const httpStatus = require("http-status-codes");
+const router = require("./routes");
+const contentTypes = require("./contentType");
+const util = require("util");
