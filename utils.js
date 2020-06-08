49791ccde08c2a0989fb7f7d60e272178f74baca
Godsgift ThankGod Akari <giftakari@yahoo.com>
2020-06-08 12:15:30 +1000

    modified project

--- utils.js
+++ utils.js
@@ -1,13 +1,15 @@
+"use strict";
+
 const fs = require("fs");
 const httpStatus = require("http-status-codes");
 const contentType = require("./contentType");
 
 module.exports = {
   getFile: (file, res) => {
-    fs.readFile(`./fs${file}`, (error, data) => {
+    fs.readFile(`./${file}`, (error, data) => {
       if (error) {
         res.writeHead(httpStatus.INTERNAL_SERVER_ERROR, contentType.html);
-        res.end("there was an error");
+        res.end("There was an error serving content!");
       }
       res.end(data);
     });
